bbaaddca50db1331a8758e487afe3ca037568d62
Ivan-Stancic <ivanstancic983@gmail.com>
2022-06-10 10:05:46 +0200

    Add project files.

---
+++ Klase/Banka.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model.Klase
+{
+    public class Banka
+    {
+        public string Naziv { get; set; }
+        public string Simbol { get; set; }
+    }
+}
---
+++ Podaci.cs
@@ -0,0 +1,34 @@
+﻿using Ispit.Model.Klase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Model
+{
+    public class Podaci
+    {
+        public List<Banka> ListaBanki = new List<Banka>()
+        {
+            new Banka { Naziv = "Zagrebacka Banka", Simbol = "ZABA" },
+            new Banka { Naziv = "Hrvatska Postanska Banka", Simbol = "HPB" },
+            new Banka { Naziv = "ReiffeisenBank Hrvatska", Simbol = "RBA" },
+        };
+
+        public List<Klijent> ListaKlijenata = new List<Klijent>()
+        {
+            new Klijent { ImePrezime = "Ivo Ivić", Stanje = 100000, Simbol = "ZABA" },
+            new Klijent { ImePrezime = "Ante antić", Stanje = 10000000, Simbol = "HPB" },
+            new Klijent { ImePrezime = "Marko Markić", Stanje = 100000, Simbol = "HPB" },
+            new Klijent { ImePrezime = "Pero Perič", Stanje = 100000, Simbol = "ZABA" },
+            new Klijent { ImePrezime = "Jan Janić", Stanje = 100000, Simbol = "HPB" },
+            new Klijent { ImePrezime = "Karla Karlić", Stanje = 100000, Simbol = "RBA" },
+            new Klijent { ImePrezime = "Marija Marić", Stanje = 10000000, Simbol = "RBA" },
+            new Klijent { ImePrezime = "Štefa Štefić", Stanje = 100000, Simbol = "HPB" },
+            new Klijent { ImePrezime = "Branko Brankić", Stanje = 100000, Simbol = "RBA" },
+            new Klijent { ImePrezime = "Mujo Mujić", Stanje = 10000000, Simbol = "ZABA" },
+        };
+
+    }
+}
